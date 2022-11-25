4216127b526f186fc4a907b4b744aba030048d8f
Pierre-Henri Kocan <pierre-henrikocan@macbook-pro-de-pierre-henri.home>
2022-11-25 10:51:21 +0100

    slug entities updated

--- src/Entity/Raid.php
+++ src/Entity/Raid.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\RaidRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,32 +23,9 @@ class Raid
     private $name;
 
     /**
-<<<<<<< HEAD
-     * @ORM\ManyToMany(targetEntity=Event::class, mappedBy="raid")
-     */
-    private $events;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Item::class, mappedBy="raid")
-     */
-    private $item;
-
-    /**
-     * @ORM\ManyToMany(targetEntity=Player::class, inversedBy="raids")
-     */
-    private $player;
-
-    public function __construct()
-    {
-        $this->events = new ArrayCollection();
-        $this->item = new ArrayCollection();
-        $this->player = new ArrayCollection();
-    }
-=======
      * @ORM\Column(type="string", length=64)
      */
     private $slug;
->>>>>>> entities
 
     public function getId(): ?int
     {
@@ -69,85 +44,6 @@ class Raid
         return $this;
     }
 
-<<<<<<< HEAD
-    /**
-     * @return Collection<int, Event>
-     */
-    public function getEvents(): Collection
-    {
-        return $this->events;
-    }
-
-    public function addEvent(Event $event): self
-    {
-        if (!$this->events->contains($event)) {
-            $this->events[] = $event;
-            $event->addRaid($this);
-        }
-
-        return $this;
-    }
-
-    public function removeEvent(Event $event): self
-    {
-        if ($this->events->removeElement($event)) {
-            $event->removeRaid($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, item>
-     */
-    public function getItem(): Collection
-    {
-        return $this->item;
-    }
-
-    public function addItem(item $item): self
-    {
-        if (!$this->item->contains($item)) {
-            $this->item[] = $item;
-            $item->setRaid($this);
-        }
-
-        return $this;
-    }
-
-    public function removeItem(item $item): self
-    {
-        if ($this->item->removeElement($item)) {
-            // set the owning side to null (unless already changed)
-            if ($item->getRaid() === $this) {
-                $item->setRaid(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, player>
-     */
-    public function getPlayer(): Collection
-    {
-        return $this->player;
-    }
-
-    public function addPlayer(player $player): self
-    {
-        if (!$this->player->contains($player)) {
-            $this->player[] = $player;
-        }
-
-        return $this;
-    }
-
-    public function removePlayer(player $player): self
-    {
-        $this->player->removeElement($player);
-=======
     public function getSlug(): ?string
     {
         return $this->slug;
@@ -156,7 +52,6 @@ class Raid
     public function setSlug(string $slug): self
     {
         $this->slug = $slug;
->>>>>>> entities
 
         return $this;
     }
--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\RoleRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,26 +23,9 @@ class Role
     private $name;
 
     /**
-<<<<<<< HEAD
-     * @ORM\ManyToMany(targetEntity=Item::class, mappedBy="role")
-     */
-    private $items;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Player::class, mappedBy="role")
-     */
-    private $players;
-
-    public function __construct()
-    {
-        $this->items = new ArrayCollection();
-        $this->players = new ArrayCollection();
-    }
-=======
      * @ORM\Column(type="string", length=64)
      */
     private $slug;
->>>>>>> entities
 
     public function getId(): ?int
     {
@@ -63,61 +44,6 @@ class Role
         return $this;
     }
 
-<<<<<<< HEAD
-    /**
-     * @return Collection<int, Item>
-     */
-    public function getItems(): Collection
-    {
-        return $this->items;
-    }
-
-    public function addItem(Item $item): self
-    {
-        if (!$this->items->contains($item)) {
-            $this->items[] = $item;
-            $item->addRole($this);
-        }
-
-        return $this;
-    }
-
-    public function removeItem(Item $item): self
-    {
-        if ($this->items->removeElement($item)) {
-            $item->removeRole($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Player>
-     */
-    public function getPlayers(): Collection
-    {
-        return $this->players;
-    }
-
-    public function addPlayer(Player $player): self
-    {
-        if (!$this->players->contains($player)) {
-            $this->players[] = $player;
-            $player->setRole($this);
-        }
-
-        return $this;
-    }
-
-    public function removePlayer(Player $player): self
-    {
-        if ($this->players->removeElement($player)) {
-            // set the owning side to null (unless already changed)
-            if ($player->getRole() === $this) {
-                $player->setRole(null);
-            }
-        }
-=======
     public function getSlug(): ?string
     {
         return $this->slug;
@@ -126,7 +52,6 @@ class Role
     public function setSlug(string $slug): self
     {
         $this->slug = $slug;
->>>>>>> entities
 
         return $this;
     }
