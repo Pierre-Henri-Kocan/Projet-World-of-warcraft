02188b72ef2b88ffadccc74c75d55b728a672c09
Pierre-Henri Kocan <pierre-henrikocan@ordinateur-portable-de-pierre-henri.home>
2022-12-13 11:09:24 +0100

    CRUDL method created in controllers

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,19 +2,96 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/event", name="app_event_")
+ */
 class EventController extends AbstractController
 {
+
     /**
-     * @Route("/event", name="app_event")
+     * @Route("/", name="list", methods={"GET"})
      */
-    public function index(): Response
+    public function list(EventRepository $eventRepository): Response
     {
         return $this->render('event/list.html.twig', [
             'controller_name' => 'EventController',
+            'events' => $eventRepository->findAll(),
         ]);
     }
+
+    /**
+     * @Route("/create", name="create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EventRepository $eventRepository): Response
+    {
+        $event = new Event();
+
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $eventRepository->add($event, true);
+
+            $this->addFlash('success', 'Evènement ajouté');
+            return $this->redirectToRoute('app_event_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('event/create.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route ("/{id<\d+>}", name="read", methods={"GET"})
+     */
+    public function read(Event $event): Response
+    {
+        return $this->render('event/read.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    /**
+     * @Route ("/{id<\d+>}/update", name="update", methods={"GET", "POST"})
+     */
+    public function update(Request $request, Event $event, EventRepository $eventRepository): Response
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $eventRepository->add($event, true);
+
+            $this->addFlash('warning', 'Evènement modifié');
+            return $this->redirectToRoute('app_event_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('event/update.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="delete", methods={"POST"})
+     */
+    public function delete(Request $request, Event $event, EventRepository $eventRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
+            $eventRepository->remove($event, true);
+        }
+
+        $this->addFlash('success', 'Evènement supprimé');
+        return $this->redirectToRoute('app_event_list', [], Response::HTTP_SEE_OTHER);
+    }
 }
+
+
--- src/Controller/RosterController.php
+++ src/Controller/RosterController.php
@@ -2,19 +2,93 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
+use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/roster", name="app_roster_")
+ */
 class RosterController extends AbstractController
 {
     /**
-     * @Route("/roster", name="app_roster")
+     * @Route("/", name="list", methods={"GET"})
      */
-    public function index(): Response
+    public function list(PlayerRepository $playerRepository): Response
     {
         return $this->render('roster/list.html.twig', [
             'controller_name' => 'RosterController',
+            'rosters' => $playerRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, PlayerRepository $playerRepository): Response
+    {
+        $player = new Player();
+
+        $form = $this->createForm(PlayerType::class, $player);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $playerRepository->add($player, true);
+
+            $this->addFlash('success', 'Joueur ajouté');
+            return $this->redirectToRoute('app_roster_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('roster/create.html.twig', [
+            'player' => $player,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route ("/{id<\d+>}", name="read", methods={"GET"})
+     */
+    public function read(Player $player): Response
+    {
+        return $this->render('roster/read.html.twig', [
+            'event' => $player,
+        ]);
+    }
+
+    /**
+     * @Route ("/{id<\d+>}/update", name="update", methods={"GET", "POST"})
+     */
+    public function update(Request $request, Player $player, PlayerRepository $playerRepository): Response
+    {
+        $form = $this->createForm(PlayerType::class, $player);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $playerRepository->add($player, true);
+
+            $this->addFlash('warning', 'Joueur modifié');
+            return $this->redirectToRoute('app_roster_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('roster/update.html.twig', [
+            'player' => $player,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="delete", methods={"POST"})
+     */
+    public function delete(Request $request, Player $event, PlayerRepository $eventRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
+            $eventRepository->remove($event, true);
+        }
+
+        $this->addFlash('success', 'Joueur supprimé');
+        return $this->redirectToRoute('app_roster_list', [], Response::HTTP_SEE_OTHER);
     }
 }
