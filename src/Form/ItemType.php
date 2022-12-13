98e21c8fd5bc82858a32bca375733902c6eaf00d
Pierre-Henri Kocan <pierre-henrikocan@ordinateur-portable-de-pierre-henri.home>
2022-12-13 12:31:14 +0100

    Forms created

---
+++ src/Form/ItemType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use App\Entity\Item;
+use App\Entity\Location;
+use App\Entity\Player;
+use App\Entity\Raid;
+use App\Entity\Role;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ItemType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('name', TextType::class, [
+            'label' => 'Nom de l\'item',
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices'  => [
+                    'Bis' => 'Bis',
+                    'Contested' => 'Contested'],
+                    'expanded' => true,
+                'multiple' => false,
+                ])
+            ->add('slug', TextType::class, [
+                'label' => 'nom-de-l-item-sluggifié',
+                ])
+            ->add('detail', TextType::class, [
+                'label' => 'URL',
+                ])
+            ->add('role',
+            EntityType::class, [
+                'class' => Role::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'required' => true,]
+            )
+            ->add('player',
+            EntityType::class, [
+                'class' => Player::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'required' => false,]
+            )
+            ->add('events',
+            EntityType::class, [
+                'class' => Event::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'required' => false,]
+            )
+            ->add('raid',
+            EntityType::class, [
+                'class' => Raid::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => true,
+                'required' => true,]
+            )
+            ->add('locations',
+            EntityType::class, [
+                'class' => Location::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'required' => true,]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Item::class,
+        ]);
+    }
+}
